c10c0c2767e7dedeefa20b7feb90d64f012d4b0c
lascapi <gitcom@isman.fr>
2019-03-12 10:26:18 +0100

    Create the GetData class

---
+++ src/main/java/com/example/GetData.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class GetData {
+
+    String data;
+
+    public GetData() {
+        data = "";
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,13 +7,17 @@ import org.glassfish.jersey.server.ResourceConfig;
 import java.io.IOException;
 import java.net.URI;
 
+import javax.xml.crypto.Data;
+
 /**
  * Main class.
  *
  */
 public class Main {
+
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static GetData data = new GetData();
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -35,6 +39,7 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        System.out.println(data.getData());
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
