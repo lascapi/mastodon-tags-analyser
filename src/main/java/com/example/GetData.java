5a0fbb86a24a96550d605d1a043357e5ecb2473e
lascapi <gitcom@isman.fr>
2019-03-13 15:10:31 +0100

    Simplify the code, refactor

--- src/main/java/com/example/GetData.java
+++ src/main/java/com/example/GetData.java
@@ -5,38 +5,58 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import java.util.ArrayList;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
 public class GetData {
 
-    // See the tuto/doc on
-    // https://jersey.github.io/documentation/latest/user-guide.html#d0e4368
-    private String data;
-    private Client client = ClientBuilder.newClient();
-    // URL of the RESTfull API
-    private WebTarget webTarget = client.target("https://mastodon.zaclys.com/api/v1/");
-    // The service I want to use
-    private WebTarget ressourceWebTarget = webTarget.path("timelines/tag/");
-    private WebTarget mastodondWebTarget = ressourceWebTarget.path("mastodon");
-    // Invocation.Builder invocationBuilder =
-    // mastodondWebTarget.request(MediaType.TEXT_PLAIN_TYPE);
-    // Build and invoke the get request in a single step
-    private Response response = mastodondWebTarget.request("text/plain").get();
+    private JsonArray jsonArray;
 
+    // Constructor
     public GetData() {
-        data = response.readEntity(String.class);
+        jsonArray = getJson("mastodon").readEntity(JsonArray.class);
+    }
+
+    public GetData(String tag) {
+        jsonArray = getJson(tag).readEntity(JsonArray.class);
+    }
+
+    private Response getJson(String tag) {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("https://mastodon.zaclys.com/api/v1/"); // URL of the RESTfull API
+        WebTarget ressourceWebTarget = webTarget.path("timelines/tag/"); // The service I want to use
+        WebTarget mastodondWebTarget = ressourceWebTarget.path(tag); // The param : the tag I search
+        Response response = mastodondWebTarget.request("xml/json").get(); // Build and invoke the get request in a
+                                                                          // single step
+        return response;
+    }
+
+    /**
+     * @return a list of Json Object
+     */
+    public ArrayList<JsonObject> getDataObjects() {
+        ArrayList<JsonObject> list = new ArrayList<JsonObject>();
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            list.add((JsonObject) jsonArray.get(i));
+        }
+        return list;
     }
 
     /**
      * @return the data
      */
-    public String getData() {
-        return data;
+    public JsonArray getData() {
+        return jsonArray;
     }
 
     /**
      * @param data the data to set
      */
-    public void setData(String data) {
-        this.data = data;
+    public void setData(JsonArray data) {
+        this.jsonArray = data;
     }
 
     @Override
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,44 +1,20 @@
 package com.example;
 
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-
 import java.io.IOException;
-import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 
 /**
  * Main class.
- *
  */
 public class Main {
 
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
-
     public static GetData data;
 
-    public static JsonObject value = Json.createObjectBuilder().add("firstName", "John").add("lastName", "Smith")
-            .add("age", 25).build();
-
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
-     * application.
-     * 
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.example package
-        final ResourceConfig rc = new ResourceConfig().packages("com.example");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
 
     /**
      * Main method.
@@ -49,18 +25,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         test();
-
-        final HttpServer server = startServer();
-        System.out.println(String.format(
-                "Jersey app started with WADL available at " + "%sapplication.wadl\nHit enter to stop it...",
-                BASE_URI));
-        System.in.read();
-        server.shutdownNow();
     }
 
     private static void test() {
-        System.out.println("==========json object firstName value = " + value.getString("firstName"));
-        data = new GetData();
-        System.out.println(data.toString());
+        data = new GetData("zaclys");
+
+        ArrayList<String> tags = new ArrayList<>(); 
+
+        ArrayList<JsonObject> listData = data.getDataObjects();
+        for (int i = 0; i < listData.size(); i++) {
+
+            JsonArray tagsArrayList = listData.get(i).getJsonArray("tags");
+            for (int j = 0; j < tagsArrayList.size(); j++) {
+                tags.add(tagsArrayList.get(j).asJsonObject().getString("name"));
+            }
+        }
+
+        Map<String,Integer> tagsCount = new HashMap<String,Integer>(); 
+        for (String tag : tags) {
+            if (tagsCount.get(tag) != null ){
+                tagsCount.put(tag, tagsCount.get(tag) + 1);
+            }else{
+                tagsCount.put(tag, 1);
+            }
+        }
+
+        System.out.println(tagsCount);
     }
 }
